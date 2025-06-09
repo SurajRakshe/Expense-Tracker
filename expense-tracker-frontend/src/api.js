b1ad87788d48c4d353a13bb3797e6be7de929fba
Asus <Asus@DESKTOP-M00A5PB>
2025-06-09 16:46:14 +0530

    🔧 Update API base URL to point to Render backend

--- expense-tracker-frontend/src/api.js
+++ expense-tracker-frontend/src/api.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api/',
+   baseURL: 'https://expense-tracker-backend-9klo.onrender.com/api/',
+ // baseURL: 'http://localhost:8080/api/',
   headers: {
     'Content-Type': 'application/json',
   },
